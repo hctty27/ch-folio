c7292568113888d1337d563e1837fd92940535f8
hc <71583114+hctty27@users.noreply.github.com>
2026-08-04 17:17:38 +0800

    feat: enable four-wheel SU7 controller

--- sources/index.js
+++ sources/index.js
@@ -2,6 +2,7 @@ import './localization/zh-CN.js'
 import './threejs-override.js'
 import { Game } from './Game/Game.js'
 import { CockpitView } from './Game/Views/CockpitView.js'
+import { SU7FourWheelController } from './Game/World/SU7FourWheelController.js'
 import consoleLog from './data/consoleLog.js'
 
 if(import.meta.env.VITE_LOG)
@@ -11,9 +12,11 @@ if(import.meta.env.VITE_LOG)
 
 const game = new Game()
 const cockpitView = new CockpitView(game)
+const su7FourWheelController = new SU7FourWheelController(game)
 
 if(import.meta.env.VITE_GAME_PUBLIC)
 {
     window.game = game
     window.cockpitView = cockpitView
+    window.su7FourWheelController = su7FourWheelController
 }
