0fe576959ad18aaef3099b9cd0f52cb6d2a34873
hc <71583114+hctty27@users.noreply.github.com>
2026-08-04 10:48:20 +0800

    feat: initialize cockpit driving view

--- sources/index.js
+++ sources/index.js
@@ -1,6 +1,7 @@
 import './localization/zh-CN.js'
 import './threejs-override.js'
 import { Game } from './Game/Game.js'
+import { CockpitView } from './Game/Views/CockpitView.js'
 import consoleLog from './data/consoleLog.js'
 
 if(import.meta.env.VITE_LOG)
@@ -8,7 +9,11 @@ if(import.meta.env.VITE_LOG)
         ...consoleLog
     )
 
+const game = new Game()
+const cockpitView = new CockpitView(game)
+
 if(import.meta.env.VITE_GAME_PUBLIC)
-    window.game = new Game()
-else
-    new Game()
+{
+    window.game = game
+    window.cockpitView = cockpitView
+}
