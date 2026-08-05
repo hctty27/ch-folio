a5a471b4f52d6d20b79b2d654bd9b009a9ea8d48
hc <71583114+hctty27@users.noreply.github.com>
2026-08-05 20:28:01 +0800

    feat: expose authoritative protocol codecs

--- packages/authoritative-physics/src/index.js
+++ packages/authoritative-physics/src/index.js
@@ -7,6 +7,26 @@ export {
 } from './input.js'
 
 export {
+    FRAME_HEADER_BYTES,
+    FRAME_TYPES,
+    INPUT_RECORD_BYTES,
+    ProtocolError,
+    STATE_RECORD_BYTES,
+    decodeErrorFrame,
+    decodeFullSyncFrame,
+    decodeHello,
+    decodeInputBatch,
+    decodeResume,
+    decodeStateFrame,
+    encodeErrorFrame,
+    encodeFullSyncFrame,
+    encodeHello,
+    encodeInputBatch,
+    encodeResume,
+    encodeStateFrame,
+} from './protocol.js'
+
+export {
     RAPIER_VERSION,
     VERSIONS,
     assertCompatibility,
